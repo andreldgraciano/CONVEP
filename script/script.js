9188e1c18a55ab114e3023567e80fe469ea8cf1a
Dedasgdias <dedasgdias@gmail.com>
2023-09-19 17:04:03 -0300

    coments

--- script/script.js
+++ script/script.js
@@ -1,21 +1,19 @@
-// Encontre o botão e o elemento que você deseja esconder
+//btn-open-menu-mobile
 const botaoAbrirMenuMobile = document.getElementById("botaoAbrirMenuMobile");
 const menuMobileOpen = document.querySelector(".navigation-mobile__open");
 
-// Adicione um ouvinte de eventos para o clique no botão
 botaoAbrirMenuMobile.addEventListener("click", function () {
-    // Altere o estilo do elemento para "none"
     botaoAbrirMenuMobile.style.display = "none";
     menuMobileOpen.style.display = "flex";
 });
 
-// Encontre o botão e o elemento que você deseja esconder
+//btn-close-menu-mobile
 const botaoFecharMenuMobile = document.getElementById("botaoFecharMenuMobile");
 const menuMobileClose = document.querySelector(".navigation-mobile__open");
 
-// Adicione um ouvinte de eventos para o clique no botão
+
 botaoFecharMenuMobile.addEventListener("click", function () {
-    // Altere o estilo do elemento para "none"
+    
     menuMobileClose.style.display = "none";
     botaoAbrirMenuMobile.style.display = "block";
 });
